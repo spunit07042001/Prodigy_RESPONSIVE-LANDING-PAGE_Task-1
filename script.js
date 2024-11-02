a71d78ba9ea63dd10b6d29a5d1e967d4447969ef
Punit Soni <85535330+spunit07042001@users.noreply.github.com>
2024-11-02 12:12:32 +0530

    script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const navbar = document.getElementById('navbar');
 
 // Function to add or remove the scrolled class based on scroll position
 window.onscroll = function () {
-    if (window.scrollY > 50) { // Adjust this value as needed
-        navbar.style.backgroundColor = '#000'; // Change background color on scroll
+      if (window.scrollY > 50) { // Adjust this value as needed
+        navbar.style.backgroundColor = '#33ffff'; // Change background color on scroll
     } else {
-        navbar.style.backgroundColor = '#333'; // Revert back to initial background color
+        navbar.style.backgroundColor = '#00cccc'; // Revert back to initial background color
     }
 };
